6bf9a6efcfdaa3540f83ef5ff62e82157ef9b449
Maxim Kim <exynil@gmail.com>
2021-12-02 17:16:24 +0600

    feat: colorize new icons

--- js/main.js
+++ js/main.js
@@ -1,59 +1,62 @@
-var buttons = document.getElementsByClassName('buttons')[0];
+var buttons = document.getElementsByClassName("buttons")[0];
 
-document.getElementById('iconname').onchange = function () {
-    search(this.value);
-}
+document.getElementById("iconname").onchange = function () {
+  search(this.value);
+};
 
 function addIcons() {
-    buttons.innerHTML = '';
-    for (let j = 0; j < icons.length; j++) {
-        let i = document.createElement('i');
-        i.className = 'mdi mdi-36px mdi-' + icons[j].name;
+  buttons.innerHTML = "";
+  for (let j = 0; j < icons.length; j++) {
+    let i = document.createElement("i");
+    i.className = "mdi mdi-36px mdi-" + icons[j].name;
 
-        i.onmouseup = function () {
-            copyText(String.fromCodePoint(parseInt(icons[j].hex, 16)))
-        }
+    i.onmouseup = function () {
+      copyText(String.fromCodePoint(parseInt(icons[j].hex, 16)));
+    };
 
-        let span = document.createElement('span');
-        span.className = 'icon';
+    let span = document.createElement("span");
+    span.className = "icon";
 
-        let button = document.createElement('button');
-        button.className = 'button';
+    let button = document.createElement("button");
+    button.className = "button";
 
-        span.appendChild(i);
-        button.appendChild(span);
-        buttons.appendChild(button);
+    if (icons[j].version.startsWith("6")) {
+      button.className += " is-info is-outlined";
     }
+
+    span.appendChild(i);
+    button.appendChild(span);
+    buttons.appendChild(button);
+  }
 }
 
 function search(name) {
-    name = name.toLowerCase()
-    for (let i = 0; i < buttons.childElementCount; i++) {
-        className = buttons.children[i].lastChild.lastChild.className
-        if (className.includes(name)) {
-            buttons.children[i].classList.remove("is-hidden")
-        }
-        else {
-            buttons.children[i].classList.add("is-hidden")
-        }
+  name = name.toLowerCase();
+  for (let i = 0; i < buttons.childElementCount; i++) {
+    className = buttons.children[i].lastChild.lastChild.className;
+    if (className.includes(name)) {
+      buttons.children[i].classList.remove("is-hidden");
+    } else {
+      buttons.children[i].classList.add("is-hidden");
     }
+  }
 }
 
 function copyText(text) {
-    var copyFrom = document.createElement('textarea');
-    copyFrom.setAttribute("style", "position:fixed;opacity:0;top:100px;left:100px;");
-    copyFrom.value = text;
-    document.body.appendChild(copyFrom);
-    copyFrom.select();
-    document.execCommand('copy');
-    var copied = document.createElement('div');
-    copied.setAttribute('class', 'notification is-success');
-    copied.appendChild(document.createTextNode('Copied to Clipboard'));
-    document.getElementById('notification').appendChild(copied);
-    setTimeout(function () {
-        document.body.removeChild(copyFrom);
-        document.getElementById('notification').removeChild(copied);
-    }, 1500);
+  var copyFrom = document.createElement("textarea");
+  copyFrom.setAttribute("style", "position:fixed;opacity:0;top:100px;left:100px;");
+  copyFrom.value = text;
+  document.body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand("copy");
+  var copied = document.createElement("div");
+  copied.setAttribute("class", "notification is-info");
+  copied.appendChild(document.createTextNode("Скопировано"));
+  document.getElementById("notification").appendChild(copied);
+  setTimeout(function () {
+    document.body.removeChild(copyFrom);
+    document.getElementById("notification").removeChild(copied);
+  }, 1500);
 }
 
-addIcons()
+addIcons();
